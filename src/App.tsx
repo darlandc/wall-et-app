ceeb71e3a442e8cb59e1d60e4107bd95793ff0b3
Darlan <darlandc@gmail.com>
2022-11-29 19:14:17 -0300

    feat: init expo react native

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App: React.FC = () => {
+  return <View>
+    <Text>Wall Et App</Text>
+  </View>;
+};
+
+export { App };
